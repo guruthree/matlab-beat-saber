a5f621c4a9220255fcc795d994ed80d138a95348
Fred <guru.three@gmail.com>
2018-10-21 10:19:57 +0100

    display samples plot

--- displaysamples.m
+++ displaysamples.m
@@ -1,20 +1,20 @@
-%%
-% % plot((1:length(Y))/Fs, Y)
-% skip = 100;
-% Y2 = movmean(Y,skip);
-% stime = (1:skip:length(Y))/Fs;
-% signal = Y2(1:skip:end,1);
-% plot(stime, signal)
-% % xlim([0 5])
-% % xlim([1 2])
-% ylim([-1 1]);
-% yl = ylim;
-% 
-% hold on
-% ph = plot(stime(1), signal(1), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
-% lh = line([1 1]*stime(1), yl, 'Color', 'k');
-% hold off
-% xl = xlim;
-% xlimits = [-1 5];
-% % xlimits = [-2 10];
-% xlim(xlimits)
+function [handle, stime, samples] = displaysamples(ax, Y, Fs, currenttime)
+
+    skip = Fs/100; % sub-sampling to clean-up plot
+    
+    Y2 = movmean(Y,skip);
+    stime = (0:skip:(length(Y)-1))/Fs;
+    samples = Y2(1:skip:end,1);
+    plot(ax, stime, samples)
+
+    k = find(currenttime >= stime, 1);
+    if isempty(k)
+        error('currenttime does not exist')
+    end
+    
+    hold(ax, 'on')
+    handle = plot(ax, stime(k), samples(k), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', [0.8500 0.3250 0.0980]);
+    hold(ax, 'off')
+    
+    ylabel(ax, 'Sample Level [-]')
+    ax.XTickLabel = [];
--- mbs.m
+++ mbs.m
@@ -5,6 +5,7 @@ clf
 %%
 
 levelfolder = '.'; % folder with Expert.json and a .ogg file
+currenttime = 0; % start time
 
 %% setup figure
 fig.Color = 'w';
@@ -19,15 +20,18 @@ fig.MenuBar = 'none';
 %% setup axes
 ax(1) = axes('Position', [0.05 0.58 0.57 0.4]); % main PSD
 ax(2) = axes('Position', [0.66 0.58 0.32 0.18]); % mini PSD
-ax(3) = axes('Position', [0.66 0.80 0.32 0.18], 'XTickLabel', []); % samples
+ax(3) = axes('Position', [0.66 0.80 0.32 0.18]); % samples
 ax(4) = axes('Position', [0.05 0.07 0.45 0.45], 'YTickLabel', [], 'ZTickLabel', []); % 3d level display
 
 for ii=1:2
     xlabel(ax(ii), 'Time (seconds)')
     ylabel(ax(ii), 'Frequency (Hz)')
 end
-ylabel(ax(3), 'Sample Level [-]')
 xlabel(ax(4), 'Time (seconds)')
 
 %% read in level
 [data, Y, Fs] = loadbs(levelfolder);
+
+%% samples plot
+
+[handles(3), stime, samples] = displaysamples(ax(3), Y, Fs, currenttime);
